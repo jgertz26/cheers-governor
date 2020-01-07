a212f40cffd6a74e6e63dce966e64bddc3b073eb
Jeremy <jgertz26@gmail.com>
2020-01-07 17:24:29 -0500

    Add route and controller for the card object.  Throw stuff in seed file

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,9 @@
 class Card < ApplicationRecord
   belongs_to :card_category
+
+  def random
+    #eventually refactor to disinclude users' custom cards
+    offset = rand(Card.count)
+    Card.offset(offset).first
+  end
 end
